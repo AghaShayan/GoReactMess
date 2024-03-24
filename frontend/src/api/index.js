a6eb9bd2ddcc526e4c06aaac96c308ebf903e1b2
AghaShayan <shayanelong@gmail.com>
2024-03-24 13:12:09 +0330

    Initial commit

---
+++ frontend/src/api/index.js
@@ -0,0 +1,32 @@
+var socket = new WebSocket("ws://localhost:8080/websocket");
+
+let connect = () => {
+
+    console.log("Attempting Connection...")
+
+    socket.onopen = () => {
+        console.log("Connected successfully.")
+    }
+
+    socket.onmessage = msg =>{
+        console.log(msg);
+    };
+
+    socket.onclose = event =>{
+        console.log("Socket Closed Connection: ", event);
+    }
+
+    socket.onerror = error => {
+        console.log("Socket Error: ", error);
+      };
+
+};
+
+let sendMsg = msg => {
+
+    console.log("sending msg: ", msg);
+    socket.send(msg);
+
+};
+
+export { connect, sendMsg };
